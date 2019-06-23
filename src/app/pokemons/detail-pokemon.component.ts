1cc2f2005274a1032691f97d8ad6c172008422cc
Nirka69 <khd69@yahoo.com>
2019-06-23 17:21:01 +0200

    maj

---
+++ src/app/pokemons/detail-pokemon.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Pokemon } from './pokemon';
+import { PokemonsService } from './pokemons.service';
+
+@Component({
+	selector: 'detail-pokemon',
+	templateUrl: './app/pokemons/detail-pokemon.component.html'
+})
+export class DetailPokemonComponent implements OnInit {
+
+	pokemons: Pokemon[] = null;
+	pokemon: Pokemon = null;
+
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private pokemonsService: PokemonsService,
+		private titleService: Title) { }
+
+	ngOnInit(): void {
+		let id = +this.route.snapshot.paramMap.get('id');
+		this.pokemonsService.getPokemon(id)
+			.subscribe(pokemon => {
+				this.pokemon = pokemon;
+				this.titleService.setTitle(`Informations sur ${pokemon.name}`);
+			});
+	}
+	delete(pokemon: Pokemon): void {
+		this.pokemonsService.deletePokemon(pokemon)
+			.subscribe(_ => this.goBack());
+	}
+
+	goBack(): void {
+		this.router.navigate(['/pokemon/all']);
+	}
+
+	goEdit(pokemon: Pokemon): void {
+		let link = ['/pokemon/edit', pokemon.id];
+		this.router.navigate(link);
+	}
+
+	
+
+}
